2baf1b7e52d1e89c25212f3a55cca0bded6a1e2d
yevhenii pohorilko <yevheniipogorelko@gmail.com>
2020-01-21 10:07:52 +0200

    added styles for header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Component } from 'react';
+import './Header.scss'
+
+export default class Header extends Component {
+
+    render () {
+        return <div className="header">
+           <h1 className="text heading">TV-APP</h1>
+           <div className="label">
+            <input type="search" placeholder="Type to Search"></input>
+            <input type="button" value="Search"></input>
+           </div>
+        </div>
+    }
+}
+
